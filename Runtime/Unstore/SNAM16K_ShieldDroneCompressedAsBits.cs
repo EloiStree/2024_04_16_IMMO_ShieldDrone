e8c8384e7ebea6879cd3d969ad995f1aff6a5b79
Éloi Strée <eloistree@gmail.com>
2024-04-25 22:17:12 +0200

    Save: 04/25/2024 10:17 PM

--- Runtime/Unstore/SNAM16K_ShieldDroneCompressedAsBits.cs
+++ Runtime/Unstore/SNAM16K_ShieldDroneCompressedAsBits.cs
@@ -1,3 +1,3 @@
 ﻿
 
-public class SNAM16K_ShieldDroneCompressedAsBits : DroneIMMO.StaticNativeArrayMono_Generic16K<STRUCT_ShieldDroneCompressedBits> { }
+public class SNAM16K_ShieldDroneCompressedAsBits : StaticNativeArrayMono_Generic16K<STRUCT_ShieldDroneCompressedBits> { }
